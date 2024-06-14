504e276f8f8f4e9bb8db104d66ce55434acd6fb5
MrLYC <imyikong@gmail.com>
2024-06-14 13:55:28 +0800

    add poetry-to-requirements

--- versifier/__main__.py
+++ versifier/__main__.py
@@ -1,10 +1,11 @@
 import logging
+from functools import partial
 from subprocess import check_call
-from typing import Callable, List
+from typing import List
 
 import click
 
-from versifier import poetry
+from versifier.poetry import Poetry
 
 logger = logging.getLogger(__name__)
 
@@ -17,23 +18,61 @@ def cli() -> None:
 @cli.command()
 @click.option("-r", "--requirements", multiple=True, default=[], help="requirements files")
 @click.option("-d", "--dev-requirements", multiple=True, default=[], help="dev requirements files")
+@click.option("-e", "--exclude", multiple=True, default=[], help="exclude packages")
 @click.option("--poetry-path", default="poetry", help="path to poetry")
 @click.option("--dry-run", is_flag=True, help="dry run")
 def requirements_to_poetry(
     requirements: List[str],
     dev_requirements: List[str],
+    exclude: List[str],
     poetry_path: str,
     dry_run: bool,
 ) -> None:
     def fake_check_call(commands: List[str]) -> None:
         logger.info("Would run: %s", commands)
 
+    poetry = Poetry(poetry_path)
     poetry.add_from_requirements_txt(
-        poetry_path,
         requirements,
         dev_requirements,
+        exclude,
         fake_check_call if dry_run else check_call,
     )
+
+
+@cli.command()
+@click.option("-o", "--output", default="", help="output file")
+@click.option("--poetry-path", default="poetry", help="path to poetry")
+@click.option("--exclude-specifiers", is_flag=True, help="exclude specifiers")
+@click.option("--include-comments", is_flag=True, help="include comments")
+@click.option("-e", "--exclude", multiple=True, default=[], help="exclude packages")
+@click.option("-d", "--include-dev-requirements", is_flag=True, help="include dev requirements")
+@click.option("-E", "--extra-requirements", multiple=True, default=[], help="extra requirements")
+def poetry_to_requirements(
+    output: str,
+    poetry_path: str,
+    exclude_specifiers: bool,
+    include_comments: bool,
+    exclude: List[str],
+    include_dev_requirements: bool,
+    extra_requirements: List[str],
+) -> None:
+    poetry = Poetry(poetry_path)
+    fn = partial(
+        poetry.export_to_requirements_txt,
+        include_specifiers=not exclude_specifiers,
+        include_comments=include_comments,
+        exclude=exclude,
+        include_dev_requirements=include_dev_requirements,
+        extra_requirements=extra_requirements,
+    )
+
+    if output == "":
+        fn(callback=print)
+
+    else:
+        with open(output, "w") as f:
+            fn(callback=lambda line: f.write(line + "\n"))
 
 
 if __name__ == "__main__":
--- versifier/poetry.py
+++ versifier/poetry.py
@@ -1,39 +1,110 @@
 import logging
+import os
+from dataclasses import dataclass
 from subprocess import check_call
-from typing import Any, Iterable, List
+from tempfile import TemporaryDirectory
+from typing import TYPE_CHECKING, Any, Iterable, List
 
 from requirements import parse as parse_requirements
+
+if TYPE_CHECKING:
+    from requirements.requirement import Requirement
 
 logger = logging.getLogger(__name__)
 
 
-def iter_requirements(requirements: List[str]) -> Iterable[str]:
-    for n in requirements:
-        with open(n, "r") as f:
-            for r in parse_requirements(f):
-                if not r.specs:
-                    yield r.name
-                else:
-                    specifier = ",".join(f"{i[0]}{i[1]}" for i in r.specs)
-                    yield f"{r.name}{specifier}"
+@dataclass
+class Poetry:
+    poetry_path: str
 
+    def _requirement_to_literal(self, r: "Requirement", include_specifier: bool = True, raw: bool = False) -> str:
+        if raw:
+            return r.line  # type: ignore
 
-def add_from_requirements_txt(
-    poetry_path: str,
-    requirements: List[str],
-    dev_requirements: List[str],
-    callback: Any = check_call,
-) -> None:
-    commands = [poetry_path, "add"]
+        if include_specifier and r.specs:
+            specifier = ",".join(f"{i[0]}{i[1]}" for i in r.specs)
+        else:
+            specifier = ""
 
-    requirements_to_add = list(iter_requirements(requirements))
+        return f"{r.name}{specifier}"
 
-    if requirements_to_add:
-        logger.info("Adding requirements: %s", requirements_to_add)
-        callback(commands + requirements_to_add)
+    def _iter_requirements(self, requirements: List[str]) -> Iterable[str]:
+        for n in requirements:
+            with open(n, "r") as f:
+                for r in parse_requirements(f):
+                    yield self._requirement_to_literal(r)
 
-    dev_requirements_to_add = list(iter_requirements(dev_requirements))
+    def add_from_requirements_txt(
+        self,
+        requirements: List[str],
+        dev_requirements: List[str],
+        exclude: Iterable[str] = (),
+        callback: Any = check_call,
+    ) -> None:
+        exclude_packages = set(exclude)
 
-    if dev_requirements_to_add:
-        logger.info("Adding dev requirements: %s", dev_requirements_to_add)
-        callback(commands + ["--dev"] + requirements_to_add)
+        commands = [self.poetry_path, "add"]
+
+        requirements_to_add = list(r for r in self._iter_requirements(requirements) if r not in exclude_packages)
+
+        if requirements_to_add:
+            logger.info("Adding requirements: %s", requirements_to_add)
+            callback(commands + requirements_to_add)
+
+        dev_requirements_to_add = list(
+            r for r in self._iter_requirements(dev_requirements) if r not in exclude_packages
+        )
+
+        if dev_requirements_to_add:
+            logger.info("Adding dev requirements: %s", dev_requirements_to_add)
+            callback(commands + ["--dev"] + requirements_to_add)
+
+    def _export_to_requirements_txt_raw(
+        self,
+        requirements_path: str,
+        include_dev_requirements: bool = False,
+        extra_requirements: Iterable[str] = (),
+    ) -> None:
+        commands = [
+            self.poetry_path,
+            "export",
+            "--format=requirements.txt",
+            "--without-hashes",
+            f"--output={requirements_path}",
+        ]
+
+        if include_dev_requirements:
+            commands.append("--dev")
+
+        for r in extra_requirements:
+            commands.append(f"--extras={r}")
+
+        check_call(commands)
+
+    def export_to_requirements_txt(
+        self,
+        include_specifiers: bool = True,
+        include_comments: bool = False,
+        include_dev_requirements: bool = False,
+        extra_requirements: Iterable[str] = (),
+        exclude: Iterable[str] = (),
+        callback: Any = print,
+    ) -> None:
+        exclude_packages = set(exclude)
+
+        with TemporaryDirectory() as td:
+            requirements_path = os.path.join(td, "requirements.txt")
+
+            self._export_to_requirements_txt_raw(
+                requirements_path,
+                include_dev_requirements=include_dev_requirements,
+                extra_requirements=extra_requirements,
+            )
+
+            with open(requirements_path, "r") as f:
+                for r in parse_requirements(f):
+                    if r.name in exclude_packages:
+                        continue
+
+                    line = self._requirement_to_literal(r, include_specifier=include_specifiers, raw=include_comments)
+                    callback(line)
